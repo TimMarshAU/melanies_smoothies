6a9a29c65b9f298692577dc3450d7a8e4214d85d
TimMarshAU <tmarsh1@gmail.com>
2024-09-03 15:06:50 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 import pandas
-import streamlit_pandas as sp
+#import streamlit_pandas as sp
 
 # Write directly to the app
 st.title("Tim is the best :white_check_mark:")
