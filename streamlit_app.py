a5f9e250ad252b965ebe14a3b360fdf80eb4a743
TimMarshAU <tmarsh1@gmail.com>
2024-09-03 14:59:59 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
-import pandas as pd
+import pandas
 #import streamlit_pandas as sp
 
 # Write directly to the app
